80e2eb18b46d708ddbb278a6d36613018910b9b4
Kraken_5491 <167525281+Kraken-Legionary@users.noreply.github.com>
2026-02-07 00:45:14 +0000

    Фиксы и добавление кнопок гортаний видео

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 # Указываем, где лежат шаблоны и статика
 app = Flask(
     __name__, 
-    template_folder="docs/templates", 
-    static_folder="docs/static"
+    template_folder="app/templates", 
+    static_folder="app/static"
 )
 
 @app.route('/')
